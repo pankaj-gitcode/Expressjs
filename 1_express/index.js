8fcfddba12220716ad12ce71c9061dd97bc05311
pankaj-gitcode <pankajaditya75@gmail.com>
2024-07-07 08:41:40 +0530

    +GET_Method +used_Reduce()

--- 1_express/index.js
+++ 1_express/index.js
@@ -1,40 +1,44 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 2000;
 
 app.use(express.json());
 
-app.listen(PORT, (req,res)=>console.log(`PORT Listening on: ${PORT}`));
+app.listen(PORT, (req, res)=>console.log(`PORT Listening on: ${PORT}`));
 
 //local DB
 const users = [{
     name: 'John',
     kidneys:[{
-        healthy: true,
-    },{
         healthy: false,
+    },{
+        healthy: true,
     }] 
 }]
 
 app.get('/', (req, res)=>{
-    const totalKidneysLenth = users.map(e=>e.kidneys.length).toString();
-    console.log(totalKidneysLenth)
-    // const totalKidneysLenth = totalKidneys.length;
+    try{
 
-    let healthyKidney = 0;
-    let unhealthyKidney = 0;
+        let healthyKidney = 0;
+        let unhealthyKidney = 0;
 
-   
-    users.map(e=>e.kidneys.map(e=>{
-        e.healthy? healthyKidney += 1 : unhealthyKidney += 1;
-    }))
+        const totalKidneyLength = users.reduce((accum, currVal)=>{
+            return accum + currVal.kidneys.length
+        }, 0);
+
+        users.forEach(e=>e.kidneys.forEach(e=>{
+            e.healthy? healthyKidney +=1 : unhealthyKidney +=1;
+        }))
+
+        res.status(500).json({
+            totalKidneyLength,
+            healthyKidney,
+            unhealthyKidney
+        })
+    }
+    catch(err){
+        res.status(500).json({err: err.message})
+    }
 
 
-
-
-    res.json({
-        totalKidneysLenth,
-        healthyKidney,
-        unhealthyKidney
-    })
 })
